8c473d0755b1adb86d4a1fc73d365dce4742846b
Wolfgang Merkt <w.merkt@gmail.com>
2017-10-30 15:25:17 +0000

    local changes to run on 16.04 with application in mind

--- scripts/fk_example.py
+++ scripts/fk_example.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pyexotica as exo
 
-exo.Setup.initRos()
+#exo.Setup.initRos()
 solver = exo.Setup.loadSolver('{exotica_baxter}/config/baxter_ik.xml')
 problem = solver.getProblem()
-scene = problem.getScene()
+#scene = problem.getScene()
 
 print("Robot DOF: ", problem.N)
 
 problem.update(np.zeros(problem.N,))
 
 # Publish transforms to visualise in rviz
-scene.getSolver().publishFrames()
+#scene.getSolver().publishFrames()
 
 # Get FK
 problem.update(np.zeros(problem.N,))
